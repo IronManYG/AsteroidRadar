e8b9b5a630bd70864aafd7ffca49816f71fd4dd3
IronManYG <hussain.yg2010@gmail.com>
2021-02-14 21:40:04 +0300

    Add retrofir with its queries

--- app/src/main/java/com/udacity/asteroidradar/api/NetworkUtils.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/NetworkUtils.kt
@@ -1,8 +1,14 @@
 package com.udacity.asteroidradar.api
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.Constants
 import org.json.JSONObject
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -54,4 +60,44 @@ private fun getNextSevenDaysFormattedDates(): ArrayList<String> {
     }
 
     return formattedDateList
+}
+
+/**
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
+ * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
+ * object.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(Constants.BASE_URL)
+    .build()
+
+/**
+ * A public interface that exposes the [getProperties] method
+ */
+interface NasaApiService {
+    /**
+     * Returns a Coroutine [List] of [MarsProperty] which can be fetched with await() if in a Coroutine scope.
+     * The @GET annotation indicates that the "realestate" endpoint will be requested with the GET
+     * HTTP method
+     */
+    @GET("neo/rest/v1/feed")
+    suspend fun getProperties(
+        @Query("start_date") START_DATE: String,
+        @Query("end_date") END_DATE: String,
+        @Query("api_key") API_KEY: String): List<Asteroid>
+}
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object MarsApi {
+    val RETROFIT_SERVICE : NasaApiService by lazy { retrofit.create(NasaApiService::class.java) }
 }
